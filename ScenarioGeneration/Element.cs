c171a036727d3afe398edeb9f8472c3898a53980
Astrid Konicki <anne.konicki@mymail.champlain.edu>
2025-04-17 12:30:17 -0400

    Add Element relationships

--- ScenarioGeneration/Element.cs
+++ ScenarioGeneration/Element.cs
@@ -1,5 +1,5 @@
 namespace ScenarioGeneration;
-
+using ElementMap = Dictionary<Element, List<Element>>;
 public enum Element
 {
     Fire,
@@ -8,3 +8,27 @@ public enum Element
     Poison,
     Lightning,
 }
+
+public static class ElementEffectiveness 
+{
+
+    public static ElementMap SuperEffective = new()
+        {
+            { Element.Water, new () { Element.Fire, Element.Poison }},
+            { Element.Fire, new () { Element.Ice }},
+            { Element.Ice, new() { Element.Water }}
+        };
+
+    public static ElementMap NotVeryEffective = new()
+    {
+        { Element.Fire , new() { Element.Water }},
+        { Element.Water, new () { Element.Ice }}
+    };
+
+    public static ElementMap Useless = new()
+    {
+        { Element.Ice, new() { Element.Fire}}
+    };
+}
+
+
